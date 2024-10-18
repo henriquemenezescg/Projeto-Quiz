2a0338882bc5e7d0aa3fc3a7afa50b733d7a28eb
Henrique <henriquemenezescg@gmail.com>
2024-10-18 13:57:34 -0300

    Implement question rendering and click event handling

---
+++ script.js
@@ -0,0 +1,33 @@
+// Initital Data
+let currentQuestion = 0;
+
+showQuestion();
+
+// Functions
+function showQuestion(){
+  if(questions[currentQuestion]){
+    let q = questions[currentQuestion];
+
+    document.querySelector('.scoreArea').style.display = 'none';
+    document.querySelector('.questionArea').style.display = 'block';
+
+    document.querySelector('.question').innerHTML = q.question;
+    let optionsHtml = '';
+    for(let i in q.options){
+      optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`;
+    }
+
+    document.querySelector('.options').innerHTML = optionsHtml;
+
+    document.querySelectorAll('.options .option').forEach(item => {
+      item.addEventListener('click',optionClickEvent);
+    });
+
+  }else{
+    // Acabaram as questões.
+  }
+}
+
+function optionClickEvent(e){
+    console.log("Clicou em", e.target.getAttribute('data-op'));
+}
